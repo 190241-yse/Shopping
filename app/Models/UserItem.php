818360aae4d4f7b2221d0e8b6aef533c2f886684
190241-yse <190241th@yse-c.net>
2021-02-08 12:07:11 +0900

    二回目のコミット

---
+++ app/Models/UserItem.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserItem extends Model
+{
+    protected $fillable = [
+        'amount',
+        'user_id',
+        'item_id',
+        'price',
+        'tax',
+    ];
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+}
